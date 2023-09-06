f4bce66b4481e85bca6e89d3a18937373c69f37b
derrimugisha <derrimugisha@gmail.com>
2023-09-06 15:06:30 +0300

    created a compute field

--- custom/dev/derriHospital/models/patient.py
+++ custom/dev/derriHospital/models/patient.py
@@ -10,8 +10,17 @@ class DerriHospitalPatient(models.Model):
     is_child = fields.Boolean(string="is child ?", tracking=True)
     notes = fields.Text(string="Notes", tracking=True)
     gender = fields.Selection(
-
         [("male", "Male"), ("female", "Female"), ("others", "Others")], string="Gender")
+
+    capitalized_name = fields.Char(
+        string="Capitalized Name", compute="_compute_capitalized_name")
+
+    @api.depends("name")
+    def _compute_capitalized_name(self):
+        if self.name:
+            self.capitalized_name = self.name.upper()
+        else:
+            self.capitalized_name = ""
 
     @api.onchange("age")
     def on_change_age(self):
@@ -19,5 +28,3 @@ class DerriHospitalPatient(models.Model):
             self.is_child = True
         else:
             self.is_child = False
-
-
