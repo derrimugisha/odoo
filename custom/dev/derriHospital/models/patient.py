6f50f758f973e100f267addb8a2b813a6bd03cac
derrimugisha <derrimugisha@gmail.com>
2023-09-06 12:58:15 +0300

    created on change function in the form

--- custom/dev/derriHospital/models/patient.py
+++ custom/dev/derriHospital/models/patient.py
@@ -10,3 +10,10 @@ class DerriHospitalPatient(models.Model):
     notes = fields.Text(string="Notes")
     gender = fields.Selection(
         [("male", "Male"), ("female", "Female"), ("others", "Others")], string="Gender")
+
+    @api.onchange("age")
+    def on_change_age(self):
+        if self.age <= 10:
+            self.is_child = True
+        else:
+            self.is_child = False
